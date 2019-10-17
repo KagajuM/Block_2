6f4393cd2a8fb424debce2065d132ea5777d4ac7
Melissa Kagaju <Kagaju@Melissas-MacBook-Air.local>
2019-10-17 17:03:29 +0200

    finished ui

--- exercise1.js
+++ exercise1.js
@@ -7,19 +7,53 @@ var foods = [
     {'img': 'cheese pizza.jpeg', 'text': 'Cheese Pizza','date': '03/06/19 15:47'},
   ];
 
+var drinks = [
+    {'img': 'tinto de verano.jpeg', 'text': 'Tinto de Verano','date': '03/06/19 15:47'},
+    {'img': 'smoothie.jpg', 'text': 'Glowing Green Smoothie', 'date': '03/06/19 15:47'},
+    {'img': 'oj.jpeg', 'text': 'Zumo de Naranja','date': '03/06/19 15:47'},
+];
+
+var desserts = [
+    {'img': 'cheesecake.jpg', 'text': 'Tarta de Queso','date': '03/06/19 15:47'},
+    {'img': 'croissant.jpeg', 'text': 'Croissant de Mantequilla', 'date': '03/06/19 15:47'},
+    {'img': 'ice cream.jpeg', 'text': 'Ice Cream Sandwich','date': '03/06/19 15:47'},
+    {'img': 'tiramisu.jpeg', 'text': 'Tarta de Tiramisu','date': '03/06/19 15:47'},
+   
+];
 function loadFoods() {
     console.log("Well shit happened");
     var foodContainer = document.getElementById('foods');
+    var drinkContainer = document.getElementById('drinks');
+    var dessertContainer = document.getElementById('desserts');
 
     for(var i = 0; i <foods.length; i++){
         var food = foods[i];
         var event = document.querySelector('template').content.cloneNode(true); 
         event.getElementById('img').src = food.img;
+        event.getElementById('img').alt = food.text;
         event.getElementById('text').innerText = food.text;
         event.querySelector('.date').innerText = food.date;
-        console.log("Logging event "+ i, event);
         foodContainer.appendChild(event);
     }
     
+    for(var i = 0; i <drinks.length; i++){
+        var drink = drinks[i];
+        var event = document.querySelector('template').content.cloneNode(true); 
+        event.getElementById('img').src = drink.img;
+        event.getElementById('img').alt = drink.text;
+        event.getElementById('text').innerText = drink.text;
+        event.querySelector('.date').innerText = drink.date;
+        drinkContainer.appendChild(event);
+    }
+
+    for(var i = 0; i <desserts.length; i++){
+        var dessert = desserts[i];
+        var event = document.querySelector('template').content.cloneNode(true); 
+        event.getElementById('img').src = dessert.img;
+        event.getElementById('img').alt = dessert.text;
+        event.getElementById('text').innerText = dessert.text;
+        event.querySelector('.date').innerText = dessert.date;
+        dessertContainer.appendChild(event);
+    }
 }
 
